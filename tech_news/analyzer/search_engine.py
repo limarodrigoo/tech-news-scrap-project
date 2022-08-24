9c46712543135a7439b656ed1c239c52d0d80feb
Rodrigo Lima <limarodrigoo@outlook.com>
2022-08-24 15:21:19 -0300

    Feat: Implement search_by_date method

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,10 @@
+from datetime import datetime
 from tech_news.database import search_news
 
 
 # Requisito 6
 def search_by_title(title):
-    cursor = search_news(
-        {"title": {"$regex": title, "$options": "i"}}
-    )
+    cursor = search_news({"title": {"$regex": title, "$options": "i"}})
     news_list = []
     for news in cursor:
         news_list.append((news["title"], news["url"]))
@@ -14,7 +13,17 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        date = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+        cursor = search_news({"timestamp": date})
+
+        news_list = []
+        for news in cursor:
+            news_list.append((news["title"], news["url"]))
+
+        return news_list
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
